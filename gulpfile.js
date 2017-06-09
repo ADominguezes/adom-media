3d89458720b2a18dd701672619efa40afdca7d19
Adominguezes <alberto@adominguez.es>
2017-06-09 18:29:07 +0200

    fix(bower, gulpfile, index, package, demo/index): add new gulpfile and adom-demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,123 @@
-
 /* ======================================================================================================
-* Plugins
-* ======================================================================================================*/
+ * Plugins
+ * ======================================================================================================*/
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    notify = require('gulp-notify'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    browserSync = require('browser-sync').create(),
-    imagemin = require('gulp-imagemin'),
-    clean = require('gulp-rimraf'),
-    bower = require('gulp-bower'),
-    fs = require('fs'),
-    replace = require('gulp-replace'),
-    bower = require('gulp-bower')
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  notify = require('gulp-notify'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  browserSync = require('browser-sync').create(),
+  imagemin = require('gulp-imagemin'),
+  clean = require('gulp-rimraf'),
+  bower = require('gulp-bower'),
+  fs = require('fs'),
+  replace = require('gulp-replace'),
+  bower = require('gulp-bower'),
+  superstatic = require('superstatic');
 
 
 
 /* ======================================================================================================
-* task about styles
-* ======================================================================================================*/
+ * task about styles
+ * ======================================================================================================*/
 gulp.task('styles', function () {
-    gulp.src("./*.scss")
-        //.pipe(sourcemaps.init())
-        .pipe(sass()).on('error', notify.onError(function (error) {
-            return 'Error al compilar sass.\n Detalles en la consola.\n' + error;
-        }))
-        .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
-        //.pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest("./dist/css"))
-        .pipe(browserSync.stream());
+  gulp.src("./*.scss")
+    //.pipe(sourcemaps.init())
+    .pipe(sass()).on('error', notify.onError(function (error) {
+      return 'Error al compilar sass.\n Detalles en la consola.\n' + error;
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    //.pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
 });
 
 /* ======================================================================================================
-* Task for insert estiles en el adom-media-styles.html
-* ======================================================================================================*/
+ * Task for insert styles in adom-media-styles.html
+ * ======================================================================================================*/
 gulp.task('styles-replace', ['styles'], function () {
-    return gulp.src('./adom-media-styles.html')
-        .pipe(replace(/<style>[^>]*<\/style>/, function (s) {
-            var style = fs.readFileSync('./dist/css/adom-media.css', 'utf8');
-            return '<style>\n' + style + '\n</style>';
-        }))
-        .pipe(gulp.dest('./'))
-        .pipe(browserSync.stream());
+  return gulp.src('./adom-media-styles.html')
+    .pipe(replace(/<style>[^>]*<\/style>/, function (s) {
+      var style = fs.readFileSync('./dist/css/adom-media.css', 'utf8');
+      return '<style>\n' + style + '\n</style>';
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
 });
 
 /* ======================================================================================================
-* Task for server component
-* ======================================================================================================*/
+ * Task for server component
+ * ======================================================================================================*/
 gulp.task('serve', ['styles-replace', 'styles', 'watch'], function () {
-    browserSync.init({
-        injectChanges: true,
-        files: ['./*.{html, scss}', './demo/index.html', './adom-media.js', './dist/css/adom-media.css'],
-        server: "./",
-    });
+  var mw = [
+    function (req, res, next) {
+      if ((req.url.indexOf('/bower_components') !== 0) && (req.url !== '/') && (req.url !== '/demo/index.html') && (req.url !== '/adom-media.html') && (req.url !== '/adom-media.js') && (req.url !== '/adom-media-styles.html')) {
+        req.url = 'bower_components' + req.url;
+      }
+      return superstatic({
+        config: {
+          root: 'bower_components',
+          clean: true
+        }
+      })(req, res, next);
+    },
+  ];
+  browserSync.init({
+    injectChanges: true,
+    files: ['./*.{html, scss}', './demo/index.html', './adom-media.js', './dist/css/adom-media.css'],
+    notify: true,
+    server: {
+      baseDir: "./",
+      directory: false,
+      middleware: mw
+    }
+  });
 });
 
 /* ======================================================================================================
-* watch changes in files adom-media.scss and adom-media.html
-* ======================================================================================================*/
+ * watch changes in files adom-media.scss and adom-media.html
+ * ======================================================================================================*/
 gulp.task('watch', function () {
-    gulp.watch('./adom-media.html', ['styles-replace']); // Vigila cambios en el html
-    gulp.watch('./dist/css/adom-media.css', ['styles-replace']); // Vigila cambios en el html
-    gulp.watch('./*.scss', ['styles']); // Vigila cambios en los estilos
+  gulp.watch('./adom-media.html', ['styles-replace']); // Vigila cambios en el html
+  gulp.watch('./dist/css/adom-media.css', ['styles-replace']); // Vigila cambios en el html
+  gulp.watch('./*.scss', ['styles']); // Vigila cambios en los estilos
 });
 
 /* ======================================================================================================
  * Task for install bower
  * ======================================================================================================*/
 gulp.task('bower', ['bower-remove', 'bower-cache-clean'], function () {
-    return bower();
+  return bower();
 });
 
 /* ======================================================================================================
  * task for remove package bower
  * ======================================================================================================*/
 gulp.task('bower-remove', function () {
-    return gulp.src(['./bower_components'], { read: false })
-        .pipe(clean({ force: true }));
+  return gulp.src(['./bower_components'], {
+      read: false
+    })
+    .pipe(clean({
+      force: true
+    }));
 });
 
 /* ======================================================================================================
  * Task for clean cache in bower
  * ======================================================================================================*/
 gulp.task('bower-cache-clean', function () {
-    return bower({ cmd: 'cache clean' });
+  return bower({
+    cmd: 'cache clean'
+  });
 });
 
 
 /* ======================================================================================================
-* Default Task
-* ======================================================================================================*/
+ * Default Task
+ * ======================================================================================================*/
 gulp.task('default', ['bower', 'styles', 'browser-sync', 'watch']);
